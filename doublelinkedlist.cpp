4bd8c7de5ab2fbcb898f92c3f5cc1dc6ed3c9708
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-15 10:50:35 +0700

    membuat fungsi untuk mencari posisi node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -42,10 +42,20 @@ public:
             START = newNode; 
             return; 
         }
-       
+        Node *current = START;  
+        while (current->next != NULL && nim == current->next->noMhs) {     
+            cout << "\nDuplicate roll numbers not allowed!" << endl;
+            return;
+        }
+        newNode->next = current->next;  
+        newNode->prev = current;  
+
+        if(current->next != NULL)   
+            current->next->prev = newNode;  
+        current->next = newNode;   
     }
     
-
+    
 };
 
 
